324c36ebbda686f02d5da2fb88901619cfc72ef9
Firegà <leonardograziano72@gmail.com>
2025-12-02 11:14:10 +0100

    cruelty_squad p0.2

--- mega_es_verifica/js/CS.js
+++ mega_es_verifica/js/CS.js
@@ -1,7 +1,19 @@
 function cambiaColore(){
     var x = document.getElementById("titolo");
+    var y = document.body;
     var colori = ["red", "blue", "green", "purple", "orange"];
     var coloreCasuale = colori[Math.floor(Math.random() * colori.length)];
+    var coloreCasuale2 = colori[Math.floor(Math.random() * colori.length)];
+    var coloreCasuale3 = colori[Math.floor(Math.random() * colori.length)];
     x.style.color = coloreCasuale;
+    x.style.borderColor = coloreCasuale2;
+    y.style.backgroundColor = coloreCasuale3;
 }
-setInterval(cambiaColore, 1000);
+function posizioneRandom(){
+    var x = document.getElementById("home_button");
+    x.style.top = Math.floor(Math.random() * (window.innerHeight - 50)) + "px";
+    x.style.left = Math.floor(Math.random() * (window.innerWidth - 100)) + "px";
+    
+
+}
+setInterval(cambiaColore, 100);
